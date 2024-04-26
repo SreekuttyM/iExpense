1935a9a47024ebfcddf86767a566a23fb530414b
Sreekutty Maya <sreekuttystar@gmail.com>
2024-04-26 17:57:16 +0400

    project setup

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Sreekutty Maya on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
